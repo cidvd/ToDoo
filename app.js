8f2ed40d4311c3213b18963bf94bf68aea4e533b
vimscape <97984276+vimscape@users.noreply.github.com>
2022-02-28 20:15:30 +0100

    add new todo item styles and add remove function

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const placeholders = ["Walking my fish", "Eating my dog", "Hate Pooptin", "Dye my parrot", "Feeding my car", "Buy wirecard", "Sell CCIV", "Stalk with Palantir", "Short FACC", "Buy Nestle", "Vote for Karl Marx"];
 const input = document.querySelector("#input");
 const interaction = document.querySelector("#userInteraction");
-const todolist = document.querySelector("#todoList");
+const todoList = document.querySelector("#todoList");
+
+
 pholder(placeholders, input);
+
 
 interaction.addEventListener('submit', function(e){
 	e.preventDefault();
 	const newTodo = document.createElement("LI");
+    const todoMove = document.createElement("img");
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todoItem");
+    todoMove.src = "./images/moveRe.png";
+    todoMove.classList.add("todoItemInnerImg");
 	newTodo.innerText = input.value;
-	todoList.append(newTodo);
+    newTodo.classList.add("todoItemInner");
+    todoList.append(todoDiv);
+	todoDiv.appendChild(newTodo);
+    todoDiv.appendChild(todoMove);
+    del();
 });
+function del(){
 
+const delBtn = document.querySelector(".todoItemInnerImg");
+delBtn.onclick = () => {
+    todoList.remove(".todoItem");
+};
+}
 function pholder(placeList, input){
 	let num = Math.floor(Math.random() * placeList.length);
 	return input.placeholder = placeholders[num];
