c9eb189318335abf0b974632d3a608022b6fef71
devluki <lukasz.zabiegli@gmail.com>
2022-03-01 18:41:47 +0100

    Remove list items fix

--- app.js
+++ app.js
@@ -7,29 +7,35 @@ const todoList = document.querySelector("#todoList");
 pholder(placeholders, input);
 
 
-interaction.addEventListener('submit', function(e){
-	e.preventDefault();
-	const newTodo = document.createElement("LI");
+interaction.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const newTodo = document.createElement("LI");
     const todoMove = document.createElement("img");
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todoItem");
     todoMove.src = "./images/moveRe.png";
     todoMove.classList.add("todoItemInnerImg");
-	newTodo.innerText = input.value;
+    newTodo.innerText = input.value;
     newTodo.classList.add("todoItemInner");
     todoList.append(todoDiv);
-	todoDiv.appendChild(newTodo);
+    todoDiv.appendChild(newTodo);
     todoDiv.appendChild(todoMove);
     del();
 });
-function del(){
 
-const delBtn = document.querySelector(".todoItemInnerImg");
-delBtn.onclick = () => {
-    todoList.remove(".todoItem");
-};
+function del() {
+
+
+    const delBtns = document.querySelectorAll(".todoItemInnerImg");
+    delBtns.forEach(delBtn => delBtn.addEventListener('click', function () {
+        if (!delBtn.parentElement.classList.contains('todoItem')) return
+
+        this.parentElement.remove();
+    }))
+
 }
-function pholder(placeList, input){
-	let num = Math.floor(Math.random() * placeList.length);
-	return input.placeholder = placeholders[num];
+
+function pholder(placeList, input) {
+    let num = Math.floor(Math.random() * placeList.length);
+    return input.placeholder = placeholders[num];
 }
